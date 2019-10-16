1d55e76c8d9e2a79ab640c1de7b3853fe5a62a95
Eric <ericsarpong@hotmail.com>
2019-10-15 21:00:29 -0400

    updated properties

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export interface IStyleConfig {
   background?: string;
   color?: string;
   font_size?: string;
-  font_style?: string;
-  font_weight: FontWeight;
+  font_family?: string;
+  font_weight?: FontWeight;
 }
 
 export enum FontWeight {
