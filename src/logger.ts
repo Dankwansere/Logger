0bb37074dff4231b06a3447abcc33a7ddea657b9
Eric <ericsarpong@hotmail.com>
2019-10-20 13:09:09 -0400

    clean up code

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router, RouterEvent } from '@angular/router';
 import { BaseLogger } from './baseLogger';
 import { IStyleConfig } from './config';
 import { ILogger } from './Ilogger';
@@ -23,15 +23,8 @@ export class Logger extends BaseLogger implements ILogger {
   }
 
   public routeEvents(router: Router): void {
-    router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        console.log('Navigation start true');
+    router.events.subscribe( (event: RouterEvent) => {
         this.routeEventsHandler(event);
-      }
-      if (event instanceof NavigationEnd) {
-        console.log('Navigation end true');
-        this.routeEventsHandler(event);
-      }
     });
   }
 }
