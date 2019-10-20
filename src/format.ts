224e5459f5bcbaefb6580a1a49be4ad353c69294
Eric <ericsarpong@hotmail.com>
2019-10-20 11:43:11 -0400

    added generateRouteMessage() for routing messages

--- src/format.ts
+++ src/format.ts
@@ -1,3 +1,4 @@
+import { GuardsCheckEnd, GuardsCheckStart, NavigationCancel, NavigationEnd, NavigationStart, ResolveEnd, ResolveStart, RoutesRecognized } from '@angular/router';
 import { IStyleConfig } from './config';
 
 export interface IInfo {
@@ -27,14 +28,62 @@ export class Formatter {
     };
   }
 
-  public static getStyle(styleObj: IStyleConfig): string {
-    let customizedStyle: string = '';
-    for (const style in styleObj) {
-      if (style) {
-        customizedStyle += style + ': ' + styleObj[style] + '; ';
+  public static generateRouteMessage(routeEvent): string {
+    let message: string = '';
+    switch (routeEvent) {
+      case routeEvent instanceof(NavigationStart): {
+        message = `Route navigation started
+        Navigating to: ${routeEvent.url}
+        Navigation Trigger: ${routeEvent.navigationTrigger}
+        Route ID: ${routeEvent.id} `;        
+        break;
+      }
+      case routeEvent instanceof(RoutesRecognized): {
+        message = 'Route recognized';
+        break;
+      }
+      case routeEvent instanceof(GuardsCheckStart): {
+        message = 'Guard check start';
+        break;
+      }
+      case routeEvent instanceof(GuardsCheckEnd): {
+        message = 'Guard check successful';
+        break;
+      }
+      case routeEvent instanceof(ResolveStart): {
+        message = 'Resolver execution start'
+        break;
+      }
+      case routeEvent instanceof(ResolveEnd): {
+        message = 'Resolver execution successful'
+        break;
+      }
+      case routeEvent instanceof(NavigationCancel): {
+        message = `Navigation to ${routeEvent.url} cancelled.
+        Route ID: ${routeEvent.id}
+        Possible cause: This can happen when Route Guard returns a false or a redirect has been iniated.
+        `;
+        break;
+      }
+      case routeEvent instanceof(NavigationEnd): {
+        message = `Route navigation started
+        Navigating to: ${routeEvent.url}
+        Navigation Trigger: ${routeEvent.navigationTrigger}
+        Route ID: ${routeEvent.id} `; 
+        break;
       }
     }
-    return customizedStyle;
+    return message;
+  }
+
+  public static getStyle(styleObj: IStyleConfig): string {
+    let customStyle: string = '';
+    for (const style in styleObj) {
+      if (style) {
+        customStyle += style + ': ' + styleObj[style] + '; ';
+      }
+    }
+    return customStyle;
   }
 
   /**
