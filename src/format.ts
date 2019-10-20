b57a10158d5e48f87716b2e21b95f1b5aa54ff7b
Eric <ericsarpong@hotmail.com>
2019-10-20 11:44:48 -0400

    1.0.7

--- src/format.ts
+++ src/format.ts
@@ -1,4 +1,13 @@
-import { GuardsCheckEnd, GuardsCheckStart, NavigationCancel, NavigationEnd, NavigationStart, ResolveEnd, ResolveStart, RoutesRecognized } from '@angular/router';
+import {
+  GuardsCheckEnd,
+  GuardsCheckStart,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationStart,
+  ResolveEnd,
+  ResolveStart,
+  RoutesRecognized,
+} from '@angular/router';
 import { IStyleConfig } from './config';
 
 export interface IInfo {
@@ -31,45 +40,45 @@ export class Formatter {
   public static generateRouteMessage(routeEvent): string {
     let message: string = '';
     switch (routeEvent) {
-      case routeEvent instanceof(NavigationStart): {
+      case routeEvent instanceof NavigationStart: {
         message = `Route navigation started
         Navigating to: ${routeEvent.url}
         Navigation Trigger: ${routeEvent.navigationTrigger}
-        Route ID: ${routeEvent.id} `;        
+        Route ID: ${routeEvent.id} `;
         break;
       }
-      case routeEvent instanceof(RoutesRecognized): {
+      case routeEvent instanceof RoutesRecognized: {
         message = 'Route recognized';
         break;
       }
-      case routeEvent instanceof(GuardsCheckStart): {
+      case routeEvent instanceof GuardsCheckStart: {
         message = 'Guard check start';
         break;
       }
-      case routeEvent instanceof(GuardsCheckEnd): {
+      case routeEvent instanceof GuardsCheckEnd: {
         message = 'Guard check successful';
         break;
       }
-      case routeEvent instanceof(ResolveStart): {
-        message = 'Resolver execution start'
+      case routeEvent instanceof ResolveStart: {
+        message = 'Resolver execution start';
         break;
       }
-      case routeEvent instanceof(ResolveEnd): {
-        message = 'Resolver execution successful'
+      case routeEvent instanceof ResolveEnd: {
+        message = 'Resolver execution successful';
         break;
       }
-      case routeEvent instanceof(NavigationCancel): {
+      case routeEvent instanceof NavigationCancel: {
         message = `Navigation to ${routeEvent.url} cancelled.
         Route ID: ${routeEvent.id}
         Possible cause: This can happen when Route Guard returns a false or a redirect has been iniated.
         `;
         break;
       }
-      case routeEvent instanceof(NavigationEnd): {
+      case routeEvent instanceof NavigationEnd: {
         message = `Route navigation started
         Navigating to: ${routeEvent.url}
         Navigation Trigger: ${routeEvent.navigationTrigger}
-        Route ID: ${routeEvent.id} `; 
+        Route ID: ${routeEvent.id} `;
         break;
       }
     }
